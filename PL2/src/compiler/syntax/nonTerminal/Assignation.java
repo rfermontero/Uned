d73f63b785f55fb90680dd2c7e25441e8935fae2
rfernandez <rfernandez@tuenti.com>
2018-05-02 15:18:15 +0200

    Keep working

--- PL2/src/compiler/syntax/nonTerminal/Assignation.java
+++ PL2/src/compiler/syntax/nonTerminal/Assignation.java
@@ -1,7 +1,28 @@
 package compiler.syntax.nonTerminal;
 
+import es.uned.lsi.compiler.semantic.type.TypeIF;
+
 public class Assignation extends AbstractSentence {
-    public Assignation() {
+    private Expression expression;
+    private String name;
+    private ArrayRange leftRange;
+    private ArrayRange rightRange;
+
+    public Assignation(String name, Expression expression) {
         super();
+        this.name = name;
+        this.expression = expression;
+    }
+
+    public Assignation(String name, ArrayRange leftRange, ArrayRange rightRange, Expression expression) {
+        super();
+        this.name = name;
+        this.expression = expression;
+        this.leftRange = leftRange;
+        this.rightRange = rightRange;
+    }
+
+    public TypeIF getReturnType() {
+        return expression.getReturnType();
     }
 }
--- PL2/src/compiler/syntax/nonTerminal/Expression.java
+++ PL2/src/compiler/syntax/nonTerminal/Expression.java
@@ -2,15 +2,99 @@ package compiler.syntax.nonTerminal;
 
 import es.uned.lsi.compiler.semantic.type.TypeIF;
 
-public abstract class Expression extends NonTerminal {
+public class Expression extends NonTerminal {
 
     private TypeIF returnType;
+    private int value;
+    private ArrayRange from;
+    private ArrayRange to;
+    private String identifier;
+    private Expression leftExpresion;
+    private Expression rightExpression;
+    private Expression singleExpression;
+    private CallFunction call;
+    private Assignation assignation;
+    private boolean isAssignation;
 
-    public Expression(TypeIF returnType) {
+    public Expression(Assignation assignation, TypeIF returnType) {
+        this.assignation = assignation;
+        this.isAssignation = true;
         this.returnType = returnType;
+    }
+
+    public Expression(int value, TypeIF returnType) {
+        this.value = value;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public Expression(String identifier, TypeIF returnType) {
+        this.identifier = identifier;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public Expression(Expression singleExpression, TypeIF returnType) {
+        this.singleExpression = singleExpression;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public Expression(Expression leftExpresion, Expression rightExpression, TypeIF returnType) {
+        this.leftExpresion = leftExpresion;
+        this.rightExpression = rightExpression;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public Expression(String identifier, ArrayRange from, ArrayRange to, TypeIF returnType) {
+        this.identifier = identifier;
+        this.from = from;
+        this.to = to;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public Expression(CallFunction call, TypeIF returnType) {
+        this.call = call;
+        this.returnType = returnType;
+        this.isAssignation = false;
+        this.returnType = returnType;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public ArrayRange getTo() {
+        return to;
+    }
+
+    public ArrayRange getFrom() {
+        return from;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Expression getLeftExpresion() {
+        return leftExpresion;
+    }
+
+    public Expression getRightExpression() {
+        return rightExpression;
+    }
+
+    public Expression getSingleExpression() {
+        return singleExpression;
     }
 
     public TypeIF getReturnType() {
         return returnType;
+    }
+
+    public boolean isAsignation() {
+        return isAssignation;
     }
 }
